161b44a401e65a5f39d4db72dde68153b32eda6e
Ignacio Tula <itula@edsm.com.ar>
2018-08-26 23:48:02 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,111 @@
+
+### CASSETERA
+### Descripción y contexto
+---
+CASSETERA (Cableoperador Antiguo Solicita Subvalorar Elementos Tecnológicamente Evolucionados en Recursos Analógicos).
+
+Sirve para crear un proceso de batch que convierta todos los archivos de video ubicados en config['rutaleer'] en el formato requerido por un cableoperador para su distribución en una señal analógica y la consiguiente aplicación de la mosca.
+
+En específico, este script sin realizarle más modificaciones permite convertir materiales de alta definición (1920x1080) de relación de aspecto 16:9, con codificación H.264 (puede ser otra admitida por FFMPEG) con contenedores MP4 o MOV en materiales MPEG2 de 8Mbps de bitrate con una relación 4:3, aplicando barras negras arriba y abajo (Letterbox) y con una imagen o gráfico superpuesto (un archivo PNG con transparencia para aplicar mosca de canal o programa).
+
+
+### Casos de uso
+
+Este pequeño script se diseñó para ser utilizado en la siguiente situación que enfrentó UNITV (El canal público universitario de la UNGS):
+
+UNITV produce enteramente su contenido en formato digital de alta definición 16:9 y consiguió espacio televisivo dentro de la grilla de un
+canal de cable local, cuya propiedad es de la misma empresa de distribución de cable. Dicho canal utiliza el estandar analógico SD PAL 4:3.
+
+Las entregas de las emisiones de UNITV debían ser en formato MPEG2 720x576 4:3 de duración de casi una hora con todos los elementos gráficos incluidos. Y el inconveniente se encontraba en el proceso de exportación y render para crear ese archivo en calidad estándar de dicha duración.
+
+Puesto que el proceso de exportar dicho video duraba mucho más tiempo que la duración del mismo, y considerando que otros materiales en alta definición y codificación H264 (que consume más recursos del procesador) se exportaban más rápido, se decidió crear una serie de procesos en batch que permitiera contar con una copia del contenido original del canal (HD 16:9) en calidad SD 4:3.
+
+Entonces el proceso de edición y export ya no requerirán realizar la aplicación de superposición de gráficos, una decodicación de un formato y su conversión a otro, con un proceso de downgrande en el medio. Sino que el proceso de edición se realiza nativamente en MPEG2 720x576 4:3.
+
+
+### Guía de usuario
+---
+
+El archivo cassetera.py puede colocarse en cualquier lugar del sistema y con permisos de ejecución. Este archivo de python está optimizado para utilzizarse en windows, pero su ejecución en linux debe ser posible sencillamente cambiando comandos horribles de windows por los de linux (move por mv, copy por cp, etc etc.)
+
+Se debe editar el mismo con un editor de texto y cambiarse los valores de las rutas de los ejecutables y de la ubicación de las carpetas.
+
+Estos son los valores que deben cambiarse:
+```
+config['rutaffmpeg'] = "C:\\DMAOPS\\FFMPEG\\ffmpeg.exe"
+config['rutaleer'] =  "D:\\Videos\\Desktop"
+config['rutainter'] = "D:\\\\inter\\"
+config['rutaexport'] = "D:\\\\export\\"
+config['moscapal'] = "C:\\DMAOPS\\ASSETS\\moscaPAL.png"
+```
+
+** Nota: Recordar que las rutas en windows deben contener doble barra por cada barra**
+    Ejemplo: _"C:\" debería escribirse "C:\\"_
+
+_rutaffmpeg_ debe apuntar a un binario o ejecutable de ffmpeg.
+_rutaleer_   debe apuntar a la carpeta donde se leeran todos los archivos .mp4 y/o .mov que se van a convertir
+_rutainter_  debe apuntar a una carpeta temporal dobde se realizará el primer proceso de conversión, se recominda que sea en una unidad de rápida lectura/escritura y con suficiente espacio. Aunque luego este programa se encargará de eliminar su contenido.
+_rutaexport_ debe apuntar a la carpeta final donde se encontrará todo el material ya convertido y con la gráfica aplicada.
+_moscapal_   debe apuntar a un archivo (preferente) PNG que contendrá una mosca o un logo. Esta imagen debe coincidir con el tamaño y resolución final de salida de este programa (por defecto debe ser 720x576)
+
+
+_config['filtroffmpeg']_ no es necesario cambiarlo, así como está permite la conversión a PAL, pero realizando ajustes se puede convertir a otros formatos deseados.
+
+_config['metadata']_ no funciona aún, puesto que la idea es poder incorporar esta metadata, pero no se encontro soporte en exiftool o ffmpeg para poder escribir metadata en archivos .mpg
+
+
+ 	
+### Guía de instalación
+---
+
+1. Descargar la última versión desde [La página de releases](https://github.com/logosfera-zero/Cassetera-proto/releases)
+2. Descomprimir el archivo _cassetera.zip_.
+3. Instalar las dependencias indicadas más abajo, y correr el comando para instalar las librerias requeridas de python.
+4. Abrir el archivo cassetera.py y cambiar la configuración explicada anteriormente.
+
+
+#### Dependencias
+
+Para utilizar Cassetera, se requieren instalar los siguientes programas:
+
+* **Python 3.5** - *(o superior)* - [Python](https://www.python.org/downloads/)
+    * Se debe instalar el gestor de paquetes PIP 
+
+* **Paquete Audiovisual** - [Logos](https://www.logos.net.ar/software/paquete-audiovisual) o en su defecto **ffmpeg** - [ffmpeg](https://www.ffmpeg.org/)
+   
+* **Librerias de Python** -   *Instalar con el siguiente comando
+    
+```
+pip install requests colorama
+```
+
+### Cómo contribuir
+---
+Esta sección explica a desarrolladores cuáles son las maneras habituales de enviar una solicitud de adhesión de nuevo código (“pull requests”), cómo declarar fallos en la herramienta y qué guías de estilo se deben usar al escribir más líneas de código. También puedes hacer un listado de puntos que se pueden mejorar de tu código para crear ideas de mejora.
+
+### Código de conducta 
+---
+El código de conducta establece las normas sociales, reglas y responsabilidades que los individuos y organizaciones deben seguir al interactuar de alguna manera con la herramienta digital o su comunidad. Es una buena práctica para crear un ambiente de respeto e inclusión en las contribuciones al proyecto. 
+
+La plataforma Github premia y ayuda a los repositorios dispongan de este archivo. Al crear CODE_OF_CONDUCT.md puedes empezar desde una plantilla sugerida por ellos. Puedes leer más sobre cómo crear un archivo de Código de Conducta (aquí)[https://help.github.com/articles/adding-a-code-of-conduct-to-your-project/]
+
+### Autor/es
+---
+* **Ignacio Tula** - (https://www.logos.net.ar/nosotros/ignacio-tula)
+
+### Información adicional
+---
+Esta es la sección que permite agregar más información de contexto al proyecto como alguna web de relevancia, proyectos similares o que hayan usado la misma tecnología.
+
+### Licencia 
+---
+[LICENCIA](https://github.com/EL-BID/Plantilla-de-repositorio/blob/master/LICENSE.md)
+
+La licencia especifica los permisos y las condiciones de uso que el desarrollador otorga a otros desarrolladores que usen y/o modifiquen la herramienta digital.
+
+Incluye en esta sección una note con el tipo de licencia otorgado a esta herramienta digital. El texto de la licencia debe estar incluído en un archivo *LICENSE.md* o *LICENSE.txt* en la carpeta raíz.
+
+Si desconoces qué tipos de licencias existen y cuál es la mejor para cada caso, te recomendamos visitar la página https://choosealicense.com/.
+
+## Limitación de responsabilidades
+
---
+++ cassetera.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python
+""" 
+Programa principal de Cassetera (Cableoperador Antigua Solicita Subvalorar 
+Elementos Tecnológicamente Evolucionados en Recursos Analógicos).
+
+Sirve para crear un proceso de batch que convierta todos los archivos de video
+ubicados en config['rutaleer'] en el formato requerido por un cableoperador
+para su distribución en una señal analógica y la consiguiente aplicación de la mosca.
+
+
+"""
+
+
+# IMPORTS (NO TOCAR)
+from colorama import Back, Fore, init
+import requests
+import json
+import os
+import sys
+from time import sleep
+from os import listdir
+from os.path import isfile, join
+import subprocess
+from subprocess import Popen 
+clear = lambda: os.system('cls')
+
+init()
+
+
+# CREDITOS
+__author__ = "Ignacio Tula"
+__copyright__ = "Ignacio Tula, Logos Consultora, Logosfera, Zero, 2018"
+__credits__ = ["Ignacio Tula", "Tamara Polo"]
+__license__ = "AGPL"
+__version__ = "1.0.1"
+__maintainer__ = "Ignacio Tula"
+__email__ = "itula@logos.net.ar"
+__status__ = "Prototipo"
+
+
+# CONFIGURACIÓN (MODIFICAR SEGÚN SEA NECESARIO)
+config = {}
+
+    #Cambiar estos valores para indiquen una ruta válida
+config['rutaffmpeg'] = "C:\\DMAOPS\\FFMPEG\\ffmpeg.exe"
+config['rutaleer'] =  "D:\\Videos\\Desktop"
+config['rutainter'] = "D:\\\\inter\\"
+config['rutaexport'] = "D:\\\\export\\"
+config['moscapal'] = "C:\\DMAOPS\\ASSETS\\moscaPAL.png"
+
+    #No cambiar este valor
+config['marcaError'] = 0
+    # Esto se puede adecuar para que sea ntsc o pal
+config['filtroffmpeg'] = '-filter:v "pad=iw:iw*3/4:(ow-iw)/2:(oh-ih)/2,scale=720:576" -c:a copy'
+config['metadata'] = '-map_metadata 0 -metadata comment=\"www.logos.net.ar ESTUVO AQUI. Mediante prototipo Cassetera. Ondemand. LOGOS\"'
+
+# OTRAS VARIABLES PARA INICIALIZAR
+listadoArchivo = []
+
+
+
+
+
+#
+#
+# DEFINICIÓN DE FUNCIONES
+#
+#
+
+
+
+#
+# MANEJO DE ARCHIVOS
+#
+
+# Guardar los archivos mp4 y mov en la lista 
+def obtenerArchivosDeDirectorio(rutaLeer):
+
+    for i in listdir(rutaLeer):    
+        
+        rutaCompletaArchivoActual = rutaLeer + "\\" + i
+        
+        if (isfile(rutaCompletaArchivoActual) and esteArchivoEsVideo(i)):
+            
+            print( Back.BLACK + Fore.WHITE + "\n Agregando " + Fore.LIGHTBLUE_EX + i + Fore.WHITE + ".")
+            listadoArchivo.append(rutaCompletaArchivoActual)
+        
+        else:
+            
+            print( Fore.RED + "\n La entrada " + i + " no es un archivo de video")
+
+    return listadoArchivo
+
+
+def crearComandoDeConversionPal(archivo):
+    cadena =  "\"" + config['rutaffmpeg'] + "\""
+    cadena += " -y -i " + "\"" + archivo + "\""
+    cadena += " " + config['filtroffmpeg'] + " -c:v libx264 -r 25 -b:v 3M -maxrate 5M -bufsize 5M -pass 1"
+    cadena += " " + "\"" +  archivo.replace(config['rutaleer'],config['rutainter']) + "\""
+    return cadena
+
+
+def crearComandoDeSuperposicionDeMosca(archivo):
+    cadena =  "\"" + config['rutaffmpeg'] + "\""
+    cadena += " -y -i " + "\"" + archivo.replace(config['rutaleer'],config['rutainter']) + "\""
+    cadena += " -i " + "\"" +  config['moscapal'] + "\"" 
+    cadena += ' -filter_complex \"[0:v][1:v]overlay = 0:0\" -c:v mpeg2video -b:v 8000k -r 25 -ar 48000 -b:a 256k'
+    cadena += " " + "\"" + archivo.replace(config['rutaleer'],config['rutaexport']).replace(".mp4","_ltbx.mpg") + "\" " + config['metadata']
+    return cadena
+
+
+
+
+
+
+# Devuelve V o F según si el nombre del archivo coincide con el de un video 
+def esteArchivoEsVideo(nombreArchivo):
+
+    if ( nombreArchivo[-3:] == "mp4" or nombreArchivo[-3:] == "MP4"):
+        return True
+    
+    elif ( nombreArchivo[-3:] == "mov" or nombreArchivo[-3:] == "MOV"):
+        return True
+    
+    else:
+        print(nombreArchivo[-3:])
+        return False
+
+
+# Resuelve la existencia de una carpeta en la ruta de lectura
+def existeRutaLectura():
+    if (os.path.isdir(config['rutaleer'])):
+        return Back.YELLOW + Fore.GREEN + " OK  " + Fore.WHITE
+        
+    else:
+        config['marcaError'] += 1
+        return Back.YELLOW + Fore.RED + "ERROR" + Fore.WHITE
+
+
+
+# Resuelve la existencia de una carpeta en la ruta temporal
+def existeRutaTemporal():
+    if (os.path.isdir(config['rutainter'])):
+        return Back.YELLOW + Fore.GREEN + " OK  " + Fore.WHITE
+        
+    else:
+        config['marcaError'] += 1
+        return Back.YELLOW + Fore.RED + "ERROR" + Fore.WHITE
+
+
+
+# Resuelve la existencia de una carpeta en la ruta de escritura final
+def existeRutaFinal():
+    if (os.path.isdir(config['rutaexport'])):
+        return Back.YELLOW + Fore.GREEN + " OK  " + Fore.WHITE
+        
+    else:
+        config['marcaError'] += 1
+        return Back.YELLOW + Fore.RED + "ERROR" + Fore.WHITE
+
+
+
+# Resuelve la existencia de una carpeta en la ruta de escritura final
+def existeRutaMoscal():
+    if (isfile(config['moscapal'])):
+        return Back.YELLOW + Fore.GREEN + " OK  " + Fore.WHITE
+        
+    else:
+        config['marcaError'] += 1
+        return Back.YELLOW + Fore.RED + "ERROR" + Fore.WHITE
+
+
+
+# Resuelve la existencia de una carpeta en la ruta de escritura final
+def existeRutaffmpeg():
+    if (isfile(config['rutaffmpeg'])):
+        return Back.YELLOW + Fore.GREEN + " OK  " + Fore.WHITE
+        
+    else:
+        config['marcaError'] += 1
+        return Back.YELLOW + Fore.RED + "ERROR" + Fore.WHITE
+
+def procesoprevio():
+    if (isfile(os.getcwd()+"//proceso1.bat")):
+        GUIpopupProcesoExistente(1)
+        subprocess.Popen("move /y proceso1.bat proceso1.bak", stdout=subprocess.PIPE, shell=True)
+        
+    
+    if (isfile(os.getcwd()+"//proceso2.bat")):
+        GUIpopupProcesoExistente(2)
+        subprocess.Popen("move /y proceso2.bat proceso2.bak", stdout=subprocess.PIPE, shell=True)
+
+
+
+#
+# MANEJO DE LA INTERFAZ DE USUARIO
+#
+
+# Imprimir encabezado inicial de interface
+def GUIencabezadoInicial():
+    print(Back.RED + "               " + Back.GREEN + Fore.BLACK + "                                                                                                         " )
+    print(Back.RED + "   CASSETERA   " + Back.GREEN + Fore.BLACK + "Cableoperador Antiguo Solicita Subvalorar Elementos Tecnológicamente Evolucionados en Recursos Analógicos" )
+    print(Back.RED + "               " + Back.GREEN + Fore.BLACK + "                                                                                                         " )
+    print(Back.BLUE +  Fore.WHITE +  "Desarrollado por LOGOS (www.logos.net.ar). Software Libre para cooperativas, emprendedores, ONG y universidades públicas")
+    Back.BLACK + Fore.WHITE
+
+def GUItituloActitividadActual(titulo):
+    print("\n")
+    print( Back.RED + Fore.WHITE + "A continuación:  " + titulo + Back.BLACK + Fore.WHITE)
+    
+
+def GUImostrarConfiguracion():
+    print("\n")
+    print( Back.CYAN +  Fore.RED + "          Cassetera da play con esta configuración actual                                 ")
+    print( Back.CYAN +  Fore.BLACK + "Ruta de lectura  : " + Fore.WHITE + config['rutaleer'] + ( " " * (85 - 19 -  len(config['rutaleer']))    ) + existeRutaLectura()  )
+    print( Back.CYAN +  Fore.BLACK + "Ruta temporal    : " + Fore.WHITE + config['rutainter'] + ( " " * (85 - 19 -  len(config['rutainter']))  ) + existeRutaTemporal() )
+    print( Back.CYAN +  Fore.BLACK + "Ruta Export Final: " + Fore.WHITE + config['rutaexport'] + ( " " * (85 - 19 -  len(config['rutaexport']))) + existeRutaFinal()    )
+    print( Back.CYAN +  Fore.BLACK + "Ruta PNG Mosca   : " + Fore.WHITE + config['moscapal'] + ( " " * (85 - 19 -  len(config['moscapal']))    ) + existeRutaMoscal()   )
+    print( Back.CYAN +  Fore.BLACK + "                                                                                          " )
+    print( Back.CYAN +  Fore.BLACK + "FFMPEG           : " + Fore.WHITE + config['rutaffmpeg'] + ( " " * (85 - 19 -  len(config['rutaffmpeg']))) + existeRutaffmpeg()   )
+
+    Back.BLACK + Fore.WHITE
+
+def GUIpopupProcesoExistente(nro):
+    print ("\n\n\n\n" + Back.RED + Fore.WHITE + (" " * 120) + "\n" + "                             YA EXISTE PROCESO " + str(nro) + " DE BATCH PREVIOS, SE BACKAPERAN LOS MISMOS                             \n" +  (" " * 120)  )
+    sleep(5.00)
+
+
+
+#
+# MANEJO DE ERROR
+#
+
+
+# Detiene la cassetera si hay error
+def detencionPorError():   
+    if (config['marcaError'] > 0):
+        print ("\n\n\n\n" + Back.RED + Fore.WHITE + (" " * 120) + "\n" + "                                   EXISTEN ERRORES EN LAS RUTAS CORRIJA LA SITUACIÓN                                   \n" +  (" " * 120)  )
+        sleep(20.00)
+        exit()
+
+
+
+
+
+# EJECUCIÓN DEL SOFTWARE
+
+clear()
+GUIencabezadoInicial()
+GUImostrarConfiguracion()
+detencionPorError()
+GUItituloActitividadActual("Leeremos los archivos de la ruta de lectura")
+sleep(5.00)
+listadoArchivo = obtenerArchivosDeDirectorio(config['rutaleer'])
+clear()
+GUIencabezadoInicial()
+procesoprevio()
+batch1 = open("proceso1.bat","w")
+batch2 = open("proceso2.bat","w")
+GUItituloActitividadActual("Crearemos el proceso de batch para convertir cada archivo detectado")
+print("\n\n")
+
+for i in listadoArchivo:
+    comando = crearComandoDeConversionPal(i)
+    batch1.write(comando + "\n")
+    print(Back.WHITE + Fore.BLACK + "\nAsi se convierte:\n" + Back.GREEN + Fore.BLACK + comando )
+    comando = crearComandoDeSuperposicionDeMosca(i)
+    print(Back.WHITE + Fore.BLACK + "\nAsi se superpone mosca:\n" + Back.RED + Fore.BLACK + comando )
+    batch2.write(comando + "\n")
+    print(Back.BLACK + Fore.WHITE + "\n\n")
+    sleep(3)
+    
+
+clear()
+GUIencabezadoInicial()
+GUItituloActitividadActual("Cerraremos los archivos del proceso de batch")
+print(Back.BLACK)
+batch1.close()
+batch2.close()
+sleep(5.00)
+
+clear()
+GUIencabezadoInicial()
+GUItituloActitividadActual("Comenzaremos la ejecución del proceso 1, se deja paso al proceso externo")
+print(Back.WHITE + Fore.RED + "")
+p = Popen("proceso1.bat", cwd=r"." )
+stdout, stderr = p.communicate()
+sleep(5.00)
+
+clear()
+GUIencabezadoInicial()
+GUItituloActitividadActual("Comenzaremos la ejecución del proceso 2, se deja paso al proceso externo")
+print(Back.WHITE + Fore.BLUE + "")
+p = Popen("proceso2.bat", cwd=r"." )
+stdout, stderr = p.communicate()
+sleep(5.00)
+
+print(Back.BLACK)
+clear()
+GUIencabezadoInicial()
+GUItituloActitividadActual("Eliminaremos los archivos temporales que fueron creados")
+subprocess.Popen("del /q \"" + config['rutainter'] + "*\"" , stdout=subprocess.PIPE, shell=True)
+sleep(5.00)
+
+
+
+
+clear()
+GUIencabezadoInicial()
+print ("\n\n\n\n" + Back.GREEN + Fore.WHITE + (" " * 120) + "\n" + "                                        EL CASSETTE A TERMINADO, PASA AL LADO B                                        \n" +  (" " * 120)  )
+print (Back.BLACK + Fore.WHITE + "Presiona la tecla ENTER para cerrar")
+a = input()
+
+
+
+
+
+
+
+
+
+
